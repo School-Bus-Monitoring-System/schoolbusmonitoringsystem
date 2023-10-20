edbcc16968e0caed46e547521b17e3291f44b5f1
Nikhil Sarwara <tymnec@gmail.com>
2023-10-20 22:24:08 +0530

    Documented: Profile

--- src/pages/Home/Profile.js
+++ src/pages/Home/Profile.js
@@ -2,7 +2,10 @@ import { useAuthValue } from "../../context/AuthContext";
 import { signOut } from "firebase/auth";
 import { auth } from "../../firebase";
 
+// This is the profile Component
 function Profile() {
+
+  // Use the useAuthValue hook to get the current user
   const { currentUser } = useAuthValue();
 
   return (
@@ -17,10 +20,13 @@ function Profile() {
           <strong>Email verified: </strong>
           {`${currentUser?.emailVerified}`}
         </p>
+
+        {/* Use the signOut function to sign out the current user */}
         <span onClick={() => signOut(auth)}>Sign Out</span>
       </div>
     </div>
   );
 }
 
+// Export the Profile component
 export default Profile;
