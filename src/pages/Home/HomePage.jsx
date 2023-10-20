f5c79097b39b4566e697cc9cf6046af90776e6db
Nikhil Sarwara <tymnec@gmail.com>
2023-10-20 22:22:39 +0530

    Documented: HomePage

--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -1,12 +1,27 @@
 import React from "react";
 import Layout from "../../components/Layout/Layout";
 import MainView from "../../sections/MainView";
+
+// Import the CookieConsent component (--save-dev) -> Dependency
 import CookieConsent from "react-cookie-consent";
 
+// This is the home page component.
 const HomePage = () => {
   return (
     <Layout>
+      {/* Attaching all main components for home page in Main View*/}
+      {/* List of Components in Main View:
+        * Navbar
+        * Status - Login or Logout
+        * OnBus
+        * View Live
+        * About
+        * Contact
+        * Footer
+      */}
       <MainView />
+
+      {/* Attaching CookieConsent */}
       <CookieConsent
         location="bottom"
         buttonText="Accept"
